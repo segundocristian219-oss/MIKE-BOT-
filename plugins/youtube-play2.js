ddd97f6c56c7eb5f7115b3be121c44f35b30310a
Sofia1269 <sofibot507@gmail.com>
2024-11-21 19:25:17 -0400

    Update youtube-play2.js

--- plugins/youtube-play2.js
+++ plugins/youtube-play2.js
@@ -23,5 +23,5 @@ await conn.sendFile(m.chat, dl_url, title + '.mp4', `*🍭 Titulo ∙* ${title}\
 }}
 handler.help = ["play2 <búsqueda>"]
 handler.tags = ["downloader"]
-handler.command = ["play24"]
+handler.command = ["play2"]
 export default handler
