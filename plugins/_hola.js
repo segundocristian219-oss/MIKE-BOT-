78d7bd215726d5d5e0d8c4f9ac27e6154bf6effc
Sofia1269 <sofibot507@gmail.com>
2025-03-04 21:35:29 -0400

    Update _hola.js

--- plugins/_hola.js
+++ plugins/_hola.js
@@ -1,17 +1,18 @@
-
-    case 'hola':
-    conn.sendPresenceUpdate('recording', m.chat);
-    await conn.sendFile(m.chat, 'https://qu.ax/HPeS.mp3', 'hola.mp3', null, m, true, { type: 'audioMessage' });
-    break;
-
-default:
-    break;
-}
-
-} catch (err) {
+let handler = async (m, { conn }) => {
+  try {
+    switch (m.text.toLowerCase()) {
+      case 'hola':
+        conn.sendPresenceUpdate('recording', m.chat);
+        await conn.sendFile(m.chat, 'https://qu.ax/HPeS.mp3', 'hola.mp3', null, m, true, { type: 'audioMessage' });
+        break;
+      
+      default:
+        break;
+    }
+  } catch (err) {
     console.error(err);
     m.reply('Ocurrió un error al enviar el audio.');
-}
+  }
 };
 
 handler.customPrefix = /^(hola)/i;
