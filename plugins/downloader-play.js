04ffb564c4bbf16b19818e0728d84483ec098ec0
Sofia1269 <sofibot507@gmail.com>
2025-05-03 00:55:03 -0400

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,94 +1,138 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
+import fetch from "node-fetch";
+import yts from 'yt-search';
+import axios from "axios";
 
-const apis = {
-  delirius: 'https://delirius-apiofc.vercel.app/',
-  siputzx: 'https://api.siputzx.my.id/api/',
-  ryzen: 'https://apidl.asepharyana.cloud/',
-  rioo: 'https://restapi.apibotwa.biz.id/',
-  random1: 'https://api.agungny.my.id/api/'
-};
+const formatosAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
 
-const handler = async (m, {conn, command, args, text, usedPrefix}) => {
-
-    if (!text) m.reply(`_*[ ⚠️ ] Agrega lo que quieres buscar*_\n\n_Ejemplo:_\n.play Marshmello Moving On`);
-
-    try { 
-        
-        let { data } = await axios.get(`${apis.delirius}search/spotify?q=${encodeURIComponent(text)}&limit=10`);
-
-        if (!data.data || data.data.length === 0) {
-            throw `_*[ ⚠️ ] No se encontraron resultados para "${text}".*_`;
-        }
-
-        const img = data.data[0].image;
-        const url = data.data[0].url;
-        const info = `⧁ 𝙏𝙄𝙏𝙐𝙇𝙊
-» ${data.data[0].title}
-﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
-⧁ 𝗗𝗨𝗥𝗔𝗖𝗜𝗢𝗡
-» ${data.data[0].duration}
-﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
-⧁  𝘼𝙍𝙏𝙄𝙎𝙏𝘼
-» ${data.data[0].artist}
-
-_*🎶 Enviando música...*_`.trim();
-
-        await conn.sendFile(m.chat, img, 'imagen.jpg', info, m);
-
-        //＼／＼／＼／＼／＼／ DESCARGAR ＼／＼／＼／＼／＼／
-        
-        try {
-            const api1 = `${apis.delirius}download/spotifydl?url=${encodeURIComponent(url)}`;
-            const response1 = await fetch(api1);
-            const result1 = await response1.json();
-            
-            const downloadUrl1 = result1.data.url;
-            await conn.sendMessage(m.chat, { audio: { url: downloadUrl1 }, fileName: 'audio.mp3', mimetype: 'audio/mpeg', caption: null, quoted: m });
-        } catch (e1) {
-            
-            try {
-                const api2 = `${apis.delirius}download/spotifydlv3?url=${encodeURIComponent(url)}`;
-                const response2 = await fetch(api2);
-                const result2 = await response2.json();
-                
-                const downloadUrl2 = result2.data.url;
-                await conn.sendMessage(m.chat, { audio: { url: downloadUrl2 }, fileName: 'audio.mp3', mimetype: 'audio/mpeg', caption: null, quoted: m });
-                
-            } catch (e2) {
-                
-                try {
-                    const api3 = `${apis.rioo}api/spotify?url=${encodeURIComponent(url)}`;
-                    const response3 = await fetch(api3);
-                    const result3 = await response3.json();
-                    
-                    const downloadUrl3 = result3.data.response;
-                    await conn.sendMessage(m.chat, { audio: { url: downloadUrl3 }, fileName: 'audio.mp3', mimetype: 'audio/mpeg', caption: null, quoted: m });
-                    
-                } catch (e3) {
-                    
-                    try {
-                        const api4 = `${apis.ryzen}api/downloader/spotify?url=${encodeURIComponent(url)}`;
-                        const response4 = await fetch(api4);
-                        const result4 = await response4.json();
-                    
-                        const downloadUrl4 = result4.link;
-                        await conn.sendMessage(m.chat, { audio: { url: downloadUrl4 }, fileName: 'audio.mp3', mimetype: 'audio/mpeg', caption: null, quoted: m });
-                    
-                    } catch (e4) {
-                        m.reply(`❌ Ocurrió un error al descargar el audio\nError:${e4.message}`);
-                    }
-                }
-            }
-        }
-
-
-    } catch (e) {
-
-        await conn.reply(m.chat, `❌ _*El comando fallo, intentalo de nuevo`, m);
-        console.log(e);
+const ddownr = {
+  descargar: async (url, formato) => {
+    if (!formatosAudio.includes(formato)) {
+      throw new Error('Formato no soportado, verifica la lista de formatos disponibles.');
     }
+
+    const config = {
+      method: 'GET',
+      url: `https://p.oceansaver.in/ajax/download.php?format=${formato}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    };
+
+    try {
+      const response = await axios.request(config);
+
+      if (response.data && response.data.success) {
+        const { id, title, info } = response.data;
+        const { image } = info;
+        const downloadUrl = await ddownr.verificarProgreso(id);
+
+        return {
+          id: id,
+          image: image,
+          title: title,
+          downloadUrl: downloadUrl
+        };
+      } else {
+        throw new Error('Fallo al obtener los detalles del video.');
+      }
+    } catch (error) {
+      console.error('Error al intentar obtener el audio desde la primera API:', error);
+      // Si la primera API falla, se intenta con la API de respaldo
+      return await ddownr.obtenerAudioDeRespaldo(url);
+    }
+  },
+  verificarProgreso: async (id) => {
+    const config = {
+      method: 'GET',
+      url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    };
+
+    try {
+      while (true) {
+        const response = await axios.request(config);
+
+        if (response.data && response.data.success && response.data.progress === 1000) {
+          return response.data.download_url;
+        }
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      }
+    } catch (error) {
+      console.error('Error al verificar el progreso:', error);
+      throw error;
+    }
+  },
+
+  obtenerAudioDeRespaldo: async (url) => {
+    try {
+      const api = await (await fetch(`https://api.neoxr.eu/api/youtube?url=${url}&type=audio&quality=128kbps&apikey=GataDios`)).json();
+      const result = api.data.url;
+      return { downloadUrl: result };
+    } catch (error) {
+      console.error('Error al obtener el audio desde la API de respaldo:', error);
+      throw error;
+    }
+  }
 };
 
-handler.command = ['play'];
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  try {
+    if (!text.trim()) {
+      return conn.reply(m.chat, `Ingresa el nombre de la música que deseas descargar.`, m);
+    }
+
+    const search = await yts(text);
+    if (!search.all || search.all.length === 0) {
+      return m.reply('No se encontraron resultados para tu búsqueda.');
+    }
+
+    const videoInfo = search.all[0];
+    const { title, thumbnail, timestamp, views, ago, url } = videoInfo;
+    const vistas = formatoVistas(views);
+    const infoMessage = `> 𝙴𝙽𝚅𝙸𝙰𝙽𝙳𝙾 𝙿𝙴𝙳𝙸𝙳𝙾.`;
+    const thumb = (await conn.getFile(thumbnail))?.data;
+
+    const JT = {
+      contextInfo: {
+        externalAdReply: {
+          title: botname,
+          body: wm,
+          mediaType: 1,
+          previewType: 0,
+          mediaUrl: url,
+          sourceUrl: url,
+          thumbnail: thumb,
+          renderLargerThumbnail: true,
+        },
+      },
+    };
+
+    await conn.reply(m.chat, infoMessage, m, JT);
+
+    if (command === 'play') {
+      const downloadInfo = await ddownr.descargar(url, 'mp3');
+      const downloadUrl = downloadInfo.downloadUrl;
+
+      await conn.sendMessage(m.chat, { audio: { url: downloadUrl }, mimetype: "audio/mpeg" }, { quoted: m });
+    } else {
+      throw "Comando no reconocido.";
+    }
+  } catch (error) {
+    return m.reply(`⚠︎ Ocurrió un error: ${error.message}`);
+  }
+};
+
+handler.command = handler.help = ['play'];
+handler.tags = ['downloader'];
+
 export default handler;
+
+function formatoVistas(views) {
+  if (views >= 1000) {
+    return (views / 1000).toFixed(1) + 'k (' + views.toLocaleString() + ')';
+  } else {
+    return views.toString();
+  }
+}
