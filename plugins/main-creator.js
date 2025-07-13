bd41d020ec6283004f627a3e45833e7f69071cf4
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 00:22:28 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,83 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Sofi 👸🏻;;\nFN:Sofi 👸🏻\nORG:Sofi 👸🏻\nTITLE:\nitem1.TEL;waid=50765609370:50765609370\nitem1.X-ABLabel:Sofi 👸🏻\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Sofi 👸🏻\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+import PhoneNumber from 'awesome-phonenumber';
 
-export default handler
+let handler = async (m, { conn }) => {
+  m.react('🍷');
+
+  const imageUrl = 'https://qu.ax/tNPfx.jpg';
+  const numCreador = '5215565238431';
+  const ownerJid = numCreador + '@s.whatsapp.net';
+  const name = await conn.getName(ownerJid) || 'Alee';
+  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || `𝐒𝐨𝐲 𝐂𝐫𝐢𝐬𝐭𝐢𝐚𝐧, 𝐃𝐮𝐞𝐧̃𝐨 𝐝𝐞𝐥 𝐁𝐨𝐭 𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐨𝐭 🍷.`;
+  const empresa = '𝐂𝐫𝐢𝐬𝐭𝐢𝐚𝐧 - 𝐒𝐞𝐫𝐯𝐢𝐜𝐢𝐨𝐬 𝐭𝐞𝐜𝐧𝐨𝐥𝐨𝐠𝐢𝐜𝐨𝐬';
+  const instagramUrl = 'https://www.instagram.com/bki_hm66';
+
+  const vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:${instagramUrl}
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD`.trim();
+
+  const textbot = '𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐨𝐭 🍷';
+  const dev = 'Cristian';
+
+  await conn.sendMessage(
+    m.chat,
+    {
+contextInfo: {
+  externalAdReply: {
+    title: '🍷 𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐨𝐭 🍷',
+    body: '🍷 𝑺𝒉𝒂𝒅𝒐𝒘 𝑩𝒐𝒕 🍷',
+    mediaType: 1,
+    thumbnailUrl: 'https://qu.ax/tNPfx.jpg',
+    renderLargerThumbnail: false,
+    sourceUrl: ''
+  }
+},
+
+      contacts: {
+        displayName: name,
+        contacts: [{ vcard }]
+      },
+      contextInfo: {
+        mentionedJid: [m.sender],
+        isForwarded: true,
+        forwardingScore: 999,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: 'Alee'.id,
+          newsletterName: 'Alee'.name,
+          serverMessageId: -1,
+        },
+        externalAdReply: {
+          title: textbot,
+          body: dev,
+          thumbnailUrl: imageUrl,
+          sourceUrl: instagramUrl,
+          mediaType: 1,
+          showAdAttribution: true,
+          renderLargerThumbnail: true,
+        },
+      }
+    },
+    { quoted: m }
+  );
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
