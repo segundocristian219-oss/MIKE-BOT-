2043511b1ce954096914714e6f957df8c6ba35d7
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 14:39:02 -0600

    Actualizar group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -1,13 +1,13 @@
 const handler = async (m, { conn, text, quoted }) => {
-  const u = (text || quoted?.sender)?.replace(/\D/g, '')
-  if (!u) return m.reply('🚩 Use el comando correctamente\n\nEjemplo:\n> .promote @usuario', m)
+  const u = (text || quoted?.sender)?.replace(/\D/g, '');
+  if (!u) return m.reply('Así no', m);
 
-  await conn.groupParticipantsUpdate(m.chat, [u + '@s.whatsapp.net'], 'promote')
-}
+  await conn.groupParticipantsUpdate(m.chat, [u + '@s.whatsapp.net'], 'promote');
+};
 
-handler.command = /^(promote|daradmin|darpoder)$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
+handler.command = /^(promote|daradmin|darpoder)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
 
-export default handler
+export default handler;
