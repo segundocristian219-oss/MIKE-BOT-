0ea21b4398585f930d23b444bfe25e2964f4afd9
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 02:41:14 -0600

    Actualizar serbot.js

--- plugins/serbot.js
+++ plugins/serbot.js
@@ -272,8 +272,8 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               forwardingScore: 999,
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363419364337473@newsletter',
-                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀',
+                newsletterJid: '120363422161687949@newsletter',
+                newsletterName: 'xd',
                 serverMessageId: -1
               }
             }
@@ -287,8 +287,8 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               forwardingScore: 999,
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363419364337473@newsletter',
-                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀',
+                newsletterJid: '120363422161687949@newsletter',
+                newsletterName: 'xd',
                 serverMessageId: -1
               }
             }
@@ -303,8 +303,8 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               forwardingScore: 999,
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363419364337473@newsletter',
-                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀',
+                newsletterJid: '120363422161687949@newsletter',
+                newsletterName: 'xd',
                 serverMessageId: -1
               }
             }
@@ -455,6 +455,6 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function joinChannels(conn) {
-  await conn.newsletterFollow("120363414007802886@newsletter")
-  conn.newsletterFollow("120363419364337473@newsletter")
+  await conn.newsletterFollow("120363422161687949@newsletter")
+  conn.newsletterFollow("120363422161687949@newsletter")
 }
