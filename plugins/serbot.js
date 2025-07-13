a506d0e5a0a537b9c063f4e9a2eb41d284c20741
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 02:45:14 -0600

    Actualizar serbot.js

--- plugins/serbot.js
+++ plugins/serbot.js
@@ -273,7 +273,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363422161687949@newsletter',
-                newsletterName: 'xd',
+                newsletterName: '𝗦𝗵𝗮𝗱𝗼𝘄 𝗕𝗼𝘁 🍷',
                 serverMessageId: -1
               }
             }
@@ -288,7 +288,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363422161687949@newsletter',
-                newsletterName: 'xd',
+                newsletterName: '𝗦𝗵𝗮𝗱𝗼𝘄 𝗕𝗼𝘁 🍷',
                 serverMessageId: -1
               }
             }
@@ -304,7 +304,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363422161687949@newsletter',
-                newsletterName: 'xd',
+                newsletterName: '𝗦𝗵𝗮𝗱𝗼𝘄 𝗕𝗼𝘁 🍷',
                 serverMessageId: -1
               }
             }
