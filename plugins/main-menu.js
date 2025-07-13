dab593f1708c3549cfd97014eca997e72342c9f7
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 00:53:47 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,1 +1,16 @@
-//xd
+let handler = async (m, { conn, usedPrefix, command }) => {
+  const img = 'https://qu.ax/WhnpY.jpg';
+  const name = await conn.getName(m.sender);
+  const text = `🪙 𝐌 𝐔 𝐋 𝐓 𝐈 - 𝐌 𝐄 𝐍 𝐔́ 
+
+
+ `.trim();
+
+  await conn.sendMessage(m.chat, { image: { url: img }, caption: text }, { quoted: m });
+};
+
+handler.customPrefix = /^(menu|menú|ayuda|help)$/i;
+handler.command = new RegExp; // para que funcione sin prefijo
+handler.register = true;
+
+export default handler;
