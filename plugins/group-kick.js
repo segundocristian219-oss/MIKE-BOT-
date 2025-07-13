a9eb2e3d7bfcc782ecf7b88c82af2bf4ea5b5b0e
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 14:19:09 -0600

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,19 +1,12 @@
-let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
-	
-let kickte = `_Menciona al usuario que deseas eliminar._`
+const handler = async (m, { conn }) =>
+  m.mentionedJid?.[0] || m.quoted?.sender
+    ? (await conn.groupParticipantsUpdate(m.chat, [m.mentionedJid[0] || m.quoted.sender], 'remove'),
+       m.reply('☠️ Intruso eliminado.'))
+    : m.reply('_Menciona al usuario que deseas eliminar._');
 
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`☠️ Instruso eliminado.`)
-}
+handler.command = ['kick', 'expulsar', 'eliminar'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
 
-handler.help = ['kick @user']
-handler.tags = ['group']
-handler.command = ['kick', 'expulsar', 'eliminar'] 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
-
-export default handler
+export default handler;
