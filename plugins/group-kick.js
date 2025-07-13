46f954838ffc07cc2fe4bf5933665ccdf79afe8a
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 14:17:26 -0600

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
 	
-let kickte = `_Menciona al usuario que deseas eliminar._`, m, rcanal)
+let kickte = `_Menciona al usuario que deseas eliminar._`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
