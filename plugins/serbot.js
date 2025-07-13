15b90958f3ab44a7446084b8f9d4f6cf97fc9df6
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 02:30:33 -0600

    Actualizar serbot.js

--- plugins/serbot.js
+++ plugins/serbot.js
@@ -272,8 +272,8 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               forwardingScore: 999,
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363419364337473@newsletter',
-                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀',
+                newsletterJid: '120363422161687949@newsletter',
+                newsletterName: 's',
                 serverMessageId: -1
               }
             }
@@ -287,8 +287,8 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               forwardingScore: 999,
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363419364337473@newsletter',
-                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀',
+                newsletterJid: '120363422161687949@newsletter',
+                newsletterName: 's',
                 serverMessageId: -1
               }
             }
@@ -303,8 +303,8 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               forwardingScore: 999,
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363419364337473@newsletter',
-                newsletterName: 'sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀',
+                newsletterJid: '120363422161687949@newsletter',
+                newsletterName: 's',
                 serverMessageId: -1
               }
             }
