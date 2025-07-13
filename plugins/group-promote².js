5dbf741b1cc4f5ba40dba6c011547f0f47383589
AngelDown12 <angeldown12xz@gmail.com>
2025-07-13 15:18:23 -0600

    Actualizar group-promote².js

--- plugins/group-promote².js
+++ plugins/group-promote².js
@@ -1,41 +1,18 @@
 let handler = async (m, { conn, text, quoted }) => {
-  let number;
+  let user =
+    m.mentionedJid?.[0] ||
+    quoted?.sender ||
+    (text && text.match(/\d{8,}/g)?.[0] + '@s.whatsapp.net');
 
-  if (isNaN(text) && !text.includes('@')) {
-    return conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
-  } else if (text.includes('@')) {
-    number = text.replace(/[^0-9]/g, '');
-  } else {
-    number = text;
-  }
-
-  if (!text && !quoted) {
+  if (!user || user.length > 25) {
     return conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
   }
 
-  if (number && (number.length > 13 || number.length < 8)) {
-    return conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
-  }
-
-  let user;
-
-  try {
-    if (text) {
-      user = number + "@s.whatsapp.net";
-    } else if (quoted?.sender) {
-      user = quoted.sender;
-    }
-  } catch {}
-
-  if (!user) {
-    return conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
-  }
-
-  await conn.groupParticipantsUpdate(m.chat, [user], "promote");
+  await conn.groupParticipantsUpdate(m.chat, [user], 'promote');
 };
 
-handler.command = /^promote$/i; // Solo "promote", sin prefijo
-handler.customPrefix = /^$/i;   // Obligatoriamente sin prefijo
+handler.customPrefix = /^(promote)/i; // sin prefijo, estilo: promote @
+handler.command = new RegExp;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
