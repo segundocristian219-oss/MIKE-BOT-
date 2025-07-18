d9673b019eb6ae9f5eae3f3a46d9a96877c436bc
AngelDown12 <angeldown12xz@gmail.com>
2025-07-17 23:19:02 -0600

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -5,46 +5,46 @@ export async function before(m, { conn }) {
 let img = catalogo
  global.fake = {
     contextInfo: {
-    	isForwarded: true,
+            isForwarded: true,
     forwardedNewsletterMessageInfo: {
-            newsletterJid: "120363401389725319@newsletter",
+      newsletterJid: "120363401389725319@newsletter",
       serverMessageId: 100,
-          newsletterName: '𝐁𝐔𝐔 𝐁𝐎𝐓 🔮',
+      newsletterName: '𝐁𝐔𝐔 𝐁𝐎𝐓 🔮',
     },
-	    externalAdReply: {
-				    showAdAttribution: true,
-					title: botname,
-					body: 'Hola',
-					mediaUrl: null,
-					description: null,
-					previewType: "PHOTO",
-					thumbnailUrl: 'https://files.catbox.moe/5k7vwl.jpg',
-		           sourceUrl: canal,
-		           mediaType: 1,
+            externalAdReply: {
+                                    showAdAttribution: true,
+                                        title: botname,
+                                        body: 'Hola',
+                                        mediaUrl: null,
+                                        description: null,
+                                        previewType: "PHOTO",
+                                        thumbnailUrl: 'https://files.catbox.moe/5k7vwl.jpg',
+                           sourceUrl: canal,
+                           mediaType: 1,
                    renderLargerThumbnail: false
-	    },
+            },
     },
   }
 
  global.adReply = {
-	    contextInfo: { 
+            contextInfo: { 
              forwardingScore: 9999, 
                  isForwarded: false, 
                     externalAdReply: {
-				    showAdAttribution: true,
-					title: botname,
-					body: textbot,
-					mediaUrl: null,
-					description: null,
-					previewType: "PHOTO",
-					thumbnailUrl: img,
+                                    showAdAttribution: true,
+                                        title: botname,
+                                        body: textbot,
+                                        mediaUrl: null,
+                                        description: null,
+                                        previewType: "PHOTO",
+                                        thumbnailUrl: img,
                     thumbnail: img,
-		           sourceUrl: canal,
-		           mediaType: 1,
+                           sourceUrl: canal,
+                           mediaType: 1,
                    renderLargerThumbnail: true
-				}
-			}
-		}
+                                }
+                        }
+                }
 
 global.rcanal = {
 contextInfo: {
@@ -56,13 +56,13 @@ newsletterName: '𝐁𝐔𝐔 𝐁𝐎𝐓 🔮',
 },
 externalAdReply: { 
 showAdAttribution: true,
-title: '𝐁𝐔𝐔 𝐁𝐎𝐓 🔮',
-body: '𝐁𝐔𝐔 𝐁𝐎𝐓 🔮',
+title: '𝐁𝐔𝐔 - 𝐁𝐎𝐓 🔮',
+body: '𝐂𝐫𝐢𝐬𝐭𝐢𝐚𝐧 🕷️',
 previewType: "PHOTO",
 thumbnailUrl: 'https://files.catbox.moe/5k7vwl.jpg',
-sourceUrl: 'https://files.catbox.moe/5k7vwl.jpg',
+sourceUrl: 'https://www.instagram.com/baki_hm66?igsh=cHk1eW1uZXF2ZWsy',
 mediaType: 1,
 renderLargerThumbnail: false
 },},}
-	
+
 }
