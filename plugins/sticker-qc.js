213ba7ed336286d475877bdd1c89eb336ff49c63
AngelDown12 <angeldown12xz@gmail.com>
2025-07-18 03:04:28 -0600

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,43 +1,42 @@
-import { sticker } from '../lib/sticker.js'
-import axios from 'axios'
+import { sticker } from '../lib/sticker.js';
+import axios from 'axios';
 
-let handler = async (m, { conn, text }) => {
-   if (!text) return conn.reply(m.chat, '🍭 Escribe un Texto.', m)
-   if (text.length > 30) return conn.reply(m.chat, 'Solo se permiten 30 caracteres como Máximo.', m)
-   try {
-   let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => global.imgbot.noprofileurl)
-   const obj = {
-      "type": "quote",
-      "format": "png",
-      "backgroundColor": "#FFFFFF",
-      "width": 512,
-      "height": 768,
-      "scale": 2,
-      "messages": [{
-         "entities": [],
-         "avatar": true,
-         "from": {
-            "id": 1,
-            "name": m.name,
-            "photo": {
-               "url": pp
-            }
-         },
-         "text": text,
-         "replyMessage": {}
-      }]
-   }
-   const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {
-      headers: {
-         'Content-Type': 'application/json'
-      }
-   })
-   const buffer = Buffer.from(json.data.result.image, 'base64')
-   let stick = await sticker(buffer, false, packname, author)
-   await await conn.sendFile(m.chat, stick, 'sticker.webp', '', m)
-} catch {
-}}
-handler.help = ['quotly <texto>']
-handler.tags = ['sticker']
-handler.command = ['quotly', 'qc']
-export default handler
+const handler = async (m, { conn, args }) => {
+  if (!args[0]) return;
+
+  const text = args.join(" ");
+  const pp = await conn.profilePictureUrl(m.sender).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+  const name = await conn.getName(m.sender);
+
+  const obj = {
+    type: "quote",
+    format: "png",
+    backgroundColor: "#000000",
+    width: 512,
+    height: 768,
+    scale: 2,
+    messages: [{
+      entities: [],
+      avatar: true,
+      from: {
+        id: 1,
+        name,
+        photo: { url: pp }
+      },
+      text,
+      replyMessage: {}
+    }]
+  };
+
+  const { data } = await axios.post('https://bot.lyo.su/quote/generate', obj, {
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  const buffer = Buffer.from(data.result.image, 'base64');
+  const stik = await sticker(buffer, false, '', '');
+  if (stik) return conn.sendFile(m.chat, stik, 'qc.webp', '', m);
+};
+
+handler.command = /^qc$/i;
+
+export default handler;
