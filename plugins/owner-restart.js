53fc58e9eda4905949cfde27ce642913823d5442
AngelDown12 <angeldown12xz@gmail.com>
2025-08-10 21:14:22 -0600

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,16 +1,22 @@
-import { spawn } from 'child_process'
-let handler = async (m, { conn, isROwner, text }) => {
-    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    if (conn.user.jid == conn.user.jid) {
-    await m.reply('🕐 Reiniciando Bot...')
-    process.send('reset')
-  } else throw 'eh'
-}
+//Código desarrollado por Angeldown12,
 
-handler.help = ['reiniciar']
+const handler = async (m, { conn, isROwner, text }) => {
+const datas = global
+
+if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: m})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key})
+await conn.sendMessage(m.chat, {text: `𝐋𝐢𝐬𝐭𝐨 𝐣𝐞𝐟𝐞𝐜𝐢𝐭𝐨 𝐀𝐧𝐠𝐞𝐥`, edit: key})
+//process.send('reset')
+process.exit(0); 
+}
+handler.help = ['restart'] 
 handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
-
-handler.rowner = true
-
+handler.owner = true
 export default handler
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
