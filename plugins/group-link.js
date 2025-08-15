9b87a8eade8e23abcf1c1168485b983038f2d9a6
AngelDown12 <angeldown12xz@gmail.com>
2025-08-14 22:56:44 -0600

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,8 +1,1 @@
-const handler = async (m, { conn }) =>
-  conn.sendMessage(m.chat, { text: 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat) });
-
-handler.command = ['link', 'enlace'];
-handler.group = true;
-handler.botAdmin = true;
-
-export default handler;
+n
