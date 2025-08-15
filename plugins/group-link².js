84c0f3da66d7dfe67bdc0ddf3197dc60ab5d187d
AngelDown12 <angeldown12xz@gmail.com>
2025-08-14 23:20:03 -0600

    Actualizar group-link².js

--- plugins/group-link².js
+++ plugins/group-link².js
@@ -1,16 +1,18 @@
-let handler = async (m, { conn }) => {
-try {
-let res = await conn.groupInviteCode(m.chat)
-let link = 'https://chat.whatsapp.com/' + res
-await conn.reply(m.chat, `${link}`, m)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-}}
-handler.help = ['linkgroup']
-handler.tags = ['group']
-handler.command = /^enlace|link(gro?up)?$/i
-handler.group = true
-handler.botAdmin = true
-export default handler
+var handler = async (m, { conn, args }) => {
+    try {
+        let group = m.chat;
+        let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group);
+        conn.reply(m.chat, '🔗 ' + link, m, { detectLink: true });
+    } catch (error) {
+        conn.reply(m.chat, 'Error al obtener el enlace del grupo. Asegúrate de que soy administrador y estoy en un grupo.', m);
+    }
+}
+
+handler.help = ['link'];
+handler.tags = ['grupo'];
+handler.command = ['link', 'linkgroup'];
+
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
