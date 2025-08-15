72f62332670904f3eb5a1dd768f85f0bc7b36a3f
AngelDown12 <angeldown12xz@gmail.com>
2025-08-14 23:16:43 -0600

    Actualizar group-link².js

--- plugins/group-link².js
+++ plugins/group-link².js
@@ -4,7 +4,8 @@ let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 conn.reply(m.chat, link, m, {detectLink: true})
 //conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
 }
-handler.command = /^link|enlace(gro?up)?$/i
-handler.group = true
-handler.botAdmin = true
+handler.customPrefix = /^(link|.link)/i;
+handler.command = new RegExp;
+handler.group = true;
+handler.admin = true;
 export default handler
