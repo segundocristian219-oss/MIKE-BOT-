78c97ed5116fec8bff3705c532dccbc7ac6ea093
AngelDown12 <angeldown12xz@gmail.com>
2025-08-14 21:51:02 -0600

    Actualizar ff-20vs20.js

--- plugins/ff-20vs20.js
+++ plugins/ff-20vs20.js
@@ -127,11 +127,12 @@ ${formatEscuadra(esc5)}
 ㅤʚ *𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒*:
 ${formatSuplentes(suplentes)}
 
-*Reacciona con:*
-> ❤️ Participar  
-> 👍 Suplente  
-> 👎 Salir  
-> ❌ (Admin) Reiniciar lista  
+*𝖲𝗈𝗅𝗈 𝗋𝖾𝖺𝖼𝖼𝗂𝗈𝗇𝖺 𝖼𝗈𝗇:*
+
+> 「 ❤️ 」𝐏𝐚𝐫𝐭𝐢𝐜𝐢𝐩𝐚𝐫  
+> 「 👍 」𝐒𝐮𝐩𝐥𝐞𝐧𝐭𝐞  
+> 「 👎 」𝐒𝐚𝐥𝐢𝐫 𝐃𝐞 𝐋𝐚 𝐋𝐢𝐬𝐭𝐚  
+> 「 ❌ 」𝐒𝐮𝐩𝐥𝐞𝐧𝐭𝐞        
 `
 }
 
