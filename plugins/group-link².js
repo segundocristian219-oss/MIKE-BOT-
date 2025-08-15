5063db0443fc2cdf51ed76abb37ae00abb26c1e3
AngelDown12 <angeldown12xz@gmail.com>
2025-08-14 23:18:46 -0600

    Actualizar group-link².js

--- plugins/group-link².js
+++ plugins/group-link².js
@@ -1,11 +1,16 @@
-let handler = async (m, { conn, args }) => {
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, link, m, {detectLink: true})
-//conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
-}
-handler.customPrefix = /^(link|.link)/i;
-handler.command = new RegExp;
-handler.group = true;
-handler.admin = true;
+let handler = async (m, { conn }) => {
+try {
+let res = await conn.groupInviteCode(m.chat)
+let link = 'https://chat.whatsapp.com/' + res
+await conn.reply(m.chat, `${link}`, m)
+} catch (e) {
+await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+}}
+handler.help = ['linkgroup']
+handler.tags = ['group']
+handler.command = /^enlace|link(gro?up)?$/i
+handler.group = true
+handler.botAdmin = true
 export default handler
