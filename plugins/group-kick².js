c57645a4f4e6145c90a2bb05aba9ac0e0b1cefc2
AngelDown12 <angeldown12xz@gmail.com>
2025-08-15 13:10:17 -0600

    Actualizar group-kick².js

--- plugins/group-kick².js
+++ plugins/group-kick².js
@@ -4,7 +4,7 @@ const handler = async (m, { conn }) =>
        m.reply('☠️ Intruso eliminado.'))
     : m.reply('_Menciona al usuario que deseas eliminar._');
 
-handler.customPrefix = /^(kick)/i;
+handler.customPrefix = /^(kick|.kick)/i;
 handler.command = new RegExp;
 handler.group = true;
 handler.admin = true;
