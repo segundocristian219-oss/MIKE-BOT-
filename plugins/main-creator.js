573304cfe3ba15df8d2424c12c0bea963c024be2
AngelDown12 <angeldown12xz@gmail.com>
2025-08-15 18:59:30 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -60,6 +60,7 @@ END:VCARD`.trim()
 
 handler.help = ['owner']
 handler.tags = ['owner']
-handler.command = /^\owner$/i
+handler.customPrefix = /^\.?(owner|.owner)$/i;
+handler.command = new RegExp();
 handler.register = false
 export default handler
