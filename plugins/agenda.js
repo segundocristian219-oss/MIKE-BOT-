8cfbbbb55f40894fb9ba8c52c632063ac8f47f73
AngelDown12 <angeldown12xz@gmail.com>
2025-08-15 19:04:51 -0600

    Crear agenda.js

---
+++ plugins/agenda.js
@@ -0,0 +1,40 @@
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
+
+let img = 'https://telegra.ph/file/daf0bc0fc3c1e4c471c5c.jpg' 
+let texto = `📕 ¡LA #AGENDASEMANAL ESTÁ AQUÍ! 📕
+
+📢 Abran paso al REY 👑 Pelea por la corona con una nueva barba para tomar el trono del emote Máquina del Tesoro Imperial. 
+
+🔨 También adueñarte del Whac-A-Mole y no olvides comer frutas y verduras con el regreso de una MP5 peligrosa. 🐰🥕
+
+¿Listo para reinar Bermuda? 
+#CelebraciónDePascua
+
+
+
+
+
+`
+
+const fkontak = {
+        "key": {
+    "participants":"0@s.whatsapp.net",
+                "remoteJid": "status@broadcast",
+                "fromMe": false,
+                "id": "Halo"
+        },
+        "message": {
+                "contactMessage": {
+                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                }
+        },
+        "participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
+global.db.data.users[m.sender].lastcofre = new Date * 1
+}
+handler.command = ['agendasemanal'] 
+handler.register = false 
+export default handler
