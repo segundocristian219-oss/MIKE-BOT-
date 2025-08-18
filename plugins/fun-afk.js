e5b8730b819a54865ee4372b0f9bf56bfb0eec44
AngelDown12 <angeldown12xz@gmail.com>
2025-08-18 16:30:37 -0600

    Actualizar fun-afk.js

--- plugins/fun-afk.js
+++ plugins/fun-afk.js
@@ -1,20 +1,12 @@
+let handler = async (m, { text, conn }) => {
+    let user = db.data.users[m.sender]
+    user.afk = + new Date
+    user.afkReason = text
+    await m.reply(`🍭 Estas en *AFK*, Motivo *${text ? ': ' + text : ''}*`)
+}
+handler.help = ['afk <razón>']
+handler.tags = ['fun']
+handler.command = ['afk']
+handler.group = false 
 
-const handler = async (m, {text}) => {
-const user = global.db.data.users[m.sender];
-user.afk = + new Date;
-user.afkReason = text;
-conn.fakeReply(m.chat, `『 ＡＦＫ 』
-
-> ᴇʟ ᴜsᴜᴀʀɪᴏ ${conn.getName(m.sender)} ᴇsᴛᴀ ɪɴᴀᴄᴛɪᴠᴏ. 
-
-*☣️ ᴍᴏᴛɪᴠᴏs :* ${text ? ': ' + text : 'paja'}`, '0@s.whatsapp.net', ``, 'status@broadcast', null, fake)
-/*m.reply(`『 ＡＦＫ 』
-
-> ᴇsᴛᴇ ᴜsᴜᴀʀɪᴏ : ${conn.getName(m.sender)} ᴇsᴛᴀ ɪɴᴀᴄᴛɪᴠᴏ. 
-*☣️ ᴍᴏᴛɪᴠᴏs :* ${text ? ': ' + text : 'paja'}`);*/
-};
-handler.help = ['afk [alasan]'];
-handler.tags = ['econ'];
-handler.command = /^afk$/i;
-handler.money = 95
-export default handler;
+export default handler
