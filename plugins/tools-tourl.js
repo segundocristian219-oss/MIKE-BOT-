5a2dde6c8ad0a1bdbdefab5bdea84dfee1f255f1
AngelDown12 <angeldown12xz@gmail.com>
2025-08-22 02:03:04 -0600

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -87,9 +87,7 @@ const handler = async (msg, { conn, command }) => {
     if (!res.data || !res.data.url) throw new Error('❌ No se pudo subir el archivo.');
 
     await conn.sendMessage(chatId, {
-      text: `➤ `𝖮𝖱𝖣𝖤𝖭 𝖤𝖩𝖤𝖢𝖴𝖳𝖠𝖣𝖠` ✅
-
-𝖠𝖱𝖢𝖧𝖨𝖵𝖮 𝖲𝖴𝖡𝖨𝖣𝖮 𝖢𝖮𝖱𝖱𝖤𝖢𝖳𝖠𝖬𝖤𝖭𝖳𝖤 𝖠𝖰𝖴𝖨 𝖳𝖨𝖤𝖭𝖤 𝖲𝖴 𝖴𝖱𝖫 𝖫𝖨𝖲𝖳𝖠\n${res.data.url}`
+      text: `✅ *Archivo subido exitosamente:*\n${res.data.url}`
     }, { quoted: msg });
 
     await conn.sendMessage(chatId, {
