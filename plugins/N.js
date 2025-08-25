6311295f803bfe8ca580db4795bdb57144a7a744
AngelDown12 <angeldown12xz@gmail.com>
2025-08-24 22:50:59 -0600

    Actualizar N.js

--- plugins/N.js
+++ plugins/N.js
@@ -2,10 +2,15 @@ let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, {
     text: "Contacto del Owner 👑",
     footer: "DS6 Meta Bot",
-    buttons: [
-      { buttonId: ".owner", buttonText: { displayText: "👤 Owner" }, type: 1 }
-    ],
-    headerType: 1
+    templateButtons: [
+      {
+        index: 1,
+        urlButton: {
+          displayText: "📞 Owner",
+          url: "https://wa.me/521XXXXXXXXXX" // pon tu número con lada
+        }
+      }
+    ]
   }, { quoted: m })
 }
 handler.command = /^botones$/i
