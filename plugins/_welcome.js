968b00a844674658129a6ccd937f53d281377dbf
segundocristian219-oss <segundocristian219@gmail.com>
2025-09-05 20:43:53 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,7 +3,7 @@ import { WAMessageStubType } from '@whiskeysockets/baileys'
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true
 
-  const defaultPP = 'https://cdn.russellxz.click/262f94ad.jpeg'
+  const defaultPP = 'https://cdn.russellxz.click/b8394812.mp4'
     const welcomeAudioUrl = 'https://qu.ax/sjtTL.opus'
   const byeAudioUrl = 'https://qu.ax/LhbNi.opus'
 
