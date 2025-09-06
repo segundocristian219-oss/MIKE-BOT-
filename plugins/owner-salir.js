ebb7c2b055baf19b6b212068635226ea5265f906
segundocristian219-oss <segundocristian219@gmail.com>
2025-09-05 20:39:28 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! BUU - 𝗕𝗢𝗧', mentions: [m.sender] }, { quoted: m })
+let pp = 'https://cdn.russellxz.click/b8394812.mp4'
+await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! 𝑭𝒂𝒃𝒙𝑺𝒂𝒊', mentions: [m.sender] }, { quoted: m })
 await conn.groupLeave(id)}
 handler.help = ['salir']
 handler.tags = ['owner']
