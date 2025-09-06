e42a9e0fe7f3cd303d4296c50560c32e2f985e9b
segundocristian219-oss <segundocristian219@gmail.com>
2025-09-05 20:36:09 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ import moment from 'moment-timezone'
 global.owner = [
   ['5217711941041', 'jid', 'sai', true],
   ['111497485226127@lid', 'lid', 'sai', true],
-  ['151600148549841@lid', 'lid', 'cristian', true],
-  ['5215565238431', 'jid', 'Cristian', true],
+  ['59627769213003@lid', 'lid', 'cristian', true],
+  ['5215561076182', 'jid', 'Cristian', true],
   ['584124472867', 'jid', 'fabian', true],
   ['142524563853479@lid', 'lid', 'fabian', true],
   ['']
