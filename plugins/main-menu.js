82d4a12878cf1236315f2aaca65bcf2f152302c4
segundocristian219-oss <segundocristian219@gmail.com>
2025-09-16 15:00:20 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -148,9 +148,9 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-    let pp = 'https://cdn.russellxz.click/5a7b9936.mp4'
+    let pp = 'https://cdn.russellxz.click/f6c66c2a.mp4'
     await conn.sendMessage(m.chat, {
-  video: { url: 'https://cdn.russellxz.click/5a7b9936.mp4' },
+  video: { url: 'https://cdn.russellxz.click/f6c66c2a.mp4' },
   caption: text.trim(),
   gifPlayback: true
 }, { quoted: m })
