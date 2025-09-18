0b5d76063ed8edf18138f6685d0db121271db222
segundocristian219-oss <segundocristian219@gmail.com>
2025-09-18 09:14:15 -0600

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,11 +1,22 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('🔒 Chat Baneado con exito.')
+    m.reply('> bueno jefecito, me iré')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
+handler.customPrefix = /^(\apagate bot de mierda|.banchat)$/i
+handler.command = new RegExp
 handler.group = true 
 handler.owner = true
 export default handler
+
+let before = async (m, { isGroup }) => {
+    if (isGroup) {
+        let chat = global.db.data.chats[m.chat]
+        if (chat.isBanned === undefined) {
+            chat.isBanned = true
+        }
+    }
+}
+export { before }
