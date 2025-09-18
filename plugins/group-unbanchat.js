b69aae71a192c42f0009a5fb273a86c4a5d62589
segundocristian219-oss <segundocristian219@gmail.com>
2025-09-18 09:15:17 -0600

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('🔓 Bot activo en este grupo.')   
+    m.reply('> ya volví jefecito')   
 }
 handler.help = ['desbanearbot']
 handler.tags = ['group']
-handler.command = ['desbanearbot', 'unbanchat']
+handler.customPrefix = /^(\.unbanchat|vuelve bot pendejo)$/i
+handler.command = new RegExp
 handler.group = true 
 handler.owner = true
 export default handler
